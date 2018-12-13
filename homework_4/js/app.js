2688c46694c74cb5ea8d2d1f05c09f4f476197a7
Tyler Nicholson <tylernicholson@Tylers-MacBook-Pro.local>
2018-12-12 19:11:15 -0700

    savings account

--- homework_4/js/app.js
+++ homework_4/js/app.js
@@ -113,3 +113,11 @@ for (let i=0;i<=100;i++){
     bank_account2 += i*2;
 };
 console.log(bank_account2);
+// SECTION E - SAVINGS ACCOUNT:
+let sum = 0;
+for (let i=0; i<1000;i++){
+    if ((i%5 == 0)||(i%3 == 0)){
+        sum += i;
+    };
+};
+console.log(sum);
