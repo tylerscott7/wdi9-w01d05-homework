21a2299ec918de54290841bb23c0a413717df151
Tyler Nicholson <tylernicholson@Tylers-MacBook-Pro.local>
2018-12-12 17:20:14 -0700

    strings

---
+++ homework_4/js/app.js
@@ -0,0 +1,40 @@
+//    Q + A
+// 1. How do we assign a value to a variable?
+// A. By initializing a variable with let or const and
+//    setting it equal to a value.
+//
+// 2. How do we change the value of a variable?
+// A. By reassigning a value to the variable with an = sign.
+//
+// 3. How do we assign an existing variable to a new variable?
+// A. By setting the new variable equal to the existing one.
+//
+// 4. Remind me, what are declare, assign, and define?
+// A. Declare: Telling the computer what data type th variable is
+//    and allocating memory for said variable.
+//    Assign: Setting up the actual value of the variable.
+//    Define: Telling the computer the data type of the variable.
+// 
+// 5. What is pseudocoding and why should you do it?
+// A. Pseudocoding is a way of logically mapping a program
+//    in plain language instead of valid javascript.
+// 
+// 6. What percentage of time should be spent thinking about
+//    how you're going to solve a problem vs actually typing 
+//    in code to solve it?
+// A. Much more time should be spent coming up with the solution
+//    than actually programming. Roughly 90% to 10%.
+//
+//    Strings
+// 1. 
+let firstVariable = "Hello World";
+firstVariable = 50;
+let secondVariable = firstVariable;
+secondVariable = "Hello";
+// Value of firstVariable should be 50
+// 2.
+let yourName = "Tyler";
+let newString = "Hello, my name is " + yourName;
+//
+//    Booleans
+// 1. 
